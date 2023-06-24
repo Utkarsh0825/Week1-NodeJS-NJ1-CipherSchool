cbd016f17c81f38ef26a7a4f013156c44646f154
Utkarsh <75620124+Utkarsh0825@users.noreply.github.com>
2023-06-24 22:45:31 +0530

    Add files via upload

---
+++ Lecture2/example.js
@@ -0,0 +1,24 @@
+// String
+// let word = "raining";
+// console.log(word.toUpperCase());
+
+// let String = 1093;
+// console.log(String);
+
+// console.log("hello".charAt(4));
+// console.log("hello and good evening".length);
+
+// Boolean
+// console.log(typeof 1);
+// console.log(typeof true);
+// console.log(typeof isNaN("five"));
+
+if (!undefined) {
+  console.log("very true!");
+} else {
+  console.log("very false!");
+}
+
+// => !null, !undefined, !NaN => true
+
+console.log("hello".includes("e"));
